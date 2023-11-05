d63cedf739c4a5196a9dc5b2afd7e48dfd089618
wenjunlin <wenjunlin@bytedance.com>
2023-11-05 13:33:00 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,13 @@
+基于 Next.js 开发的 h5 应用
+
+# 开发
+
+```bash
+# 安装依赖
+npm install
+
+# 本地运行
+npm run dev
+```
+
+然后浏览器打开 [http://localhost:3000](http://localhost:3000)
---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+"use client";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, zora } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const { chains, publicClient } = configureChains(
+  [mainnet, polygon, optimism, arbitrum, zora],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  projectId: "YOUR_PROJECT_ID",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className + " bg-slate-100"}>
+        <WagmiConfig config={wagmiConfig}>
+          <RainbowKitProvider chains={chains}>{children}</RainbowKitProvider>
+        </WagmiConfig>
+      </body>
+    </html>
+  );
+}
